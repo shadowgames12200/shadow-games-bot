27c41676228a5edde76c07c2ec449094ea8d5ba6
shadowgames12200 <shadowgames12200@gmail.com>
2026-09-09 22:09:53 +0000

    chore: add database index

---
+++ DataBaseJson/index.js
@@ -0,0 +1,57 @@
+const {
+  JsonDatabase,
+} = require("wio.db");
+
+const produtos = new JsonDatabase({
+  databasePath: "./DataBaseJson/produtos.json"
+});
+
+
+const carrinhos = new JsonDatabase({
+  databasePath: "./DataBaseJson/carrinhos.json"
+});
+
+const pagamentos = new JsonDatabase({
+  databasePath: "./DataBaseJson/pagamentos.json"
+});
+
+const pedidos = new JsonDatabase({
+  databasePath: "./DataBaseJson/pedidos.json"
+});
+
+const estatisticas = new JsonDatabase({
+  databasePath: "./DataBaseJson/estatisticas.json"
+});
+
+const avaliacoes = new JsonDatabase({
+  databasePath: "./DataBaseJson/avaliacoes.json"
+});
+
+const configuracao = new JsonDatabase({
+  databasePath: "./DataBaseJson/configuracao.json"
+});
+
+const tickets = new JsonDatabase({
+  databasePath: "./DataBaseJson/tickets.json"
+});
+
+
+
+
+
+
+
+
+
+
+
+module.exports = {
+  produtos,
+  carrinhos,
+  pagamentos,
+  pedidos,
+  configuracao,
+  estatisticas,
+  avaliacoes,
+  tickets
+}
