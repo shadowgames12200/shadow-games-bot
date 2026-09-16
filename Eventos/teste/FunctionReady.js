b08e98227ff01e4b4ba5db7c940589486adfe485
shadowgames12200 <shadowgames12200@gmail.com>
2026-09-16 23:40:39 +0000

    Change carregarCache to non-await call
    Refactor carregarCache call to remove await.

--- Eventos/teste/FunctionReady.js
+++ Eventos/teste/FunctionReady.js
@@ -96,6 +96,6 @@ module.exports = {
         console.log(`${client.user.tag} Foi iniciado \n - Atualmente ${client.guilds.cache.size} servidores!\n - Tendo acesso a ${client.channels.cache.size} canais!\n - Contendo ${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} usuarios!`)
 
         await CheckPosition(client).catch((error) => console.error('[CheckPosition]', error))
-        await carregarCache().catch((error) => console.error('[carregarCache]', error))
+        carregarCache()
     }
 }
